4c4e88195ba2a268a2a2a9186415160b98a3671c
TanishGamare <tanishgamare20220@gmail.com>
2026-09-20 14:56:21 +0530

    Changed from execlp to execvp and also added strtok and loop

--- tsh.c
+++ tsh.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
@@ -14,6 +15,18 @@ int main(int argc, char *argv[])
     // Get input from keyboard 
     fgets(buf, 1024, stdin);
 
+    // Trim newline
+    char *nl = strchr(buf, '\n');
+    if (nl) *nl = '\0';
+
+    // Split buf into args
+    char *args[50];
+    int nargs = 0;
+    args[nargs] = strtok(buf, " "); 
+    while (args[nargs] != NULL)
+    {
+        args[++nargs] = strtok(NULL, " ");
+    }
     // Run the command using system shell ><
     //system(buf);
 
@@ -28,7 +41,7 @@ int main(int argc, char *argv[])
     else 
     {
         // we are the child 
-        execlp(buf, buf, NULL);
+        execvp(args[0], args);
         
         // something went wrong with exec 
         fprintf(stderr, "coul not exec %s\n", buf);
